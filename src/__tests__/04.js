b9cab6e27dd64ec535909f42d5355d636a113eba
Max Millington <mmillington@paypal.com>
2018-11-28 11:01:25 -0800

    second argument

--- src/__tests__/04.js
+++ src/__tests__/04.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render, fireEvent, flushEffects} from 'react-testing-library'
-import Usage from '../exercises-final/04'
-// import Usage from '../exercises/04'
+// import Usage from '../exercises-final/04'
+import Usage from '../exercises/04'
 
 afterEach(() => {
   window.localStorage.removeItem('count')
@@ -29,8 +29,8 @@ test('Usage works', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=01&em=
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
